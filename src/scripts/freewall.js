0818eae8684d406c277cca56f662e4fd5d7354a2
shgtkshruch <shgtk.shruch@gmail.com>
2016-12-03 18:16:23 +0900

    Create images grid using freewall

---
+++ src/scripts/freewall.js
@@ -0,0 +1,41 @@
+(() => {
+  'use strict';
+
+  let loadedImgs = 0;
+
+  $('.gallery__img').each((i, el) => {
+    const img = new Image();
+    img.src = $(el).attr('src');
+
+    img.onload = function () {
+      loadedImgs++;
+      $(el).parent().css({
+        width: img.width,
+        height: 210,
+      });
+    }
+  });
+
+  const id = setInterval(() => {
+
+    if (loadedImgs === 20) {
+      clearInterval(id);
+
+      const wall = new Freewall('.js-container');
+      wall.reset({
+        selector: '.gallery__item',
+        cellW: 20,
+        cellH: 210,
+        gutterX: 5,
+        gutterY: 5,
+        onResize() {
+          wall.fitWidth();
+        }
+      });
+      wall.fitWidth();
+      $(window).trigger('resize');
+    }
+  }, 100);
+
+
+})();
